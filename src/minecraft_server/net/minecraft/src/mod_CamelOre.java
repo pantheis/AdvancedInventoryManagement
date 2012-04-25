e6c11be68190e1ab7b8e34e6c9c852c611cbaa63
Cj Bowman <pantheis@kaijin.com>
2012-04-25 15:46:48 -0700

    Added configuration file Removed all item declaration, only using block

--- src/minecraft_server/net/minecraft/src/mod_CamelOre.java
+++ src/minecraft_server/net/minecraft/src/mod_CamelOre.java
@@ -1,18 +1,16 @@
 package net.minecraft.src;
 
 import net.minecraft.src.forge.*;
-
 import java.io.File;
 import java.util.*;
-
-import forge.MinecraftForge;
 import net.minecraft.server.*;
+
 
 public class mod_CamelOre extends NetworkMod
 {
-	static Configuration configuration = new Configuration(new File(, "config/CamelOre.cfg"));	
-    public static final Block oreTitanium = new CamelOreBlockOre(250, 0).setHardness(0.2F).setResistance(5F).setStepSound(Block.soundStoneFootstep).setBlockName("oreTitanium");
-    public static final Item ingotTitanium = (new Item(127)).setIconIndex(0).setItemName("ingotTitanium");
+	static Configuration configuration = new Configuration(new File("config/CamelOre.cfg"));
+	static int oreTitaniumBlockID = configurationProperties();
+    public static final Block oreTitanium = new CamelOreBlockOre(oreTitaniumBlockID, 0).setHardness(0.2F).setResistance(5F).setStepSound(Block.soundStoneFootstep).setBlockName("oreTitanium");
 
     public mod_CamelOre()
     {
@@ -22,13 +20,18 @@ public class mod_CamelOre extends NetworkMod
     public void load()
     {
 //      MinecraftForgeClient.preloadTexture("/CamelMod/CamelOre/terrain.png");
-//      MinecraftForgeClient.preloadTexture("/CamelMod/CamelOre/gui/items.png");
         ModLoader.registerBlock(oreTitanium);
 //      ModLoader.addName(oreTitanium, "Titanium Ore");
-//      ModLoader.addName(ingotTitanium, "Titanium Ingot");
-        ModLoader.addRecipe(new ItemStack(ingotTitanium, 16), new Object[] {"XX", "XX", Character.valueOf('X'), Block.dirt});                
+        ModLoader.addRecipe(new ItemStack(oreTitanium, 16), new Object[] {"XX", "XX", Character.valueOf('X'), Block.dirt});                
     }
-
+    
+    public static int configurationProperties()
+    {
+    	configuration.load();
+    	oreTitaniumBlockID = Integer.parseInt(configuration.getOrCreateBlockIdProperty("Titanium Ore", 250).value);
+    	configuration.save();
+    	return oreTitaniumBlockID;
+    }
     public String getVersion()
     {
         return "1.0.0";
