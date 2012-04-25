61f7c232af86aff90d337bfdf739339b4e4b47cc
Cj Bowman <pantheis@kaijin.com>
2012-04-25 16:19:57 -0700

    Added block to creative mode list

--- src/minecraft/net/minecraft/src/CamelOreBlockOre.java
+++ src/minecraft/net/minecraft/src/CamelOreBlockOre.java
@@ -1,12 +1,24 @@
 package net.minecraft.src;
 
 import net.minecraft.src.forge.*;
+import java.util.*;
 
 public class CamelOreBlockOre extends Block implements ITextureProvider
 {
         public CamelOreBlockOre(int i, int j)
         {
                 super(i, j, Material.ground);
+        }
+
+// Commenting this out for now, not sure it's needed for having a block receive redstone
+/*        public boolean canConnectRedstone(IBlockAccess iba, int i, int j, int k, int dir)
+        {
+            return true;
+        }
+*/
+        public void addCreativeItems(ArrayList itemList)
+        {
+                itemList.add(new ItemStack(this));
         }
 
         public String getTextureFile()
