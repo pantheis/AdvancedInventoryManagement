cbb8f88e48ce25e0660d048e01fb98ff553d0006
Yancarlo Ramsey <yancarlo@gmail.com>
2012-05-02 08:10:42 -0700

    Added meta data-sensitive block orientation as in pistons and RP machines (with easier up/down placement mechanism). Tile Entity not yet aware of orientation. -Tallinu

--- src/minecraft/kaijin/InventoryStocker/BlockInventoryStocker.java
+++ src/minecraft/kaijin/InventoryStocker/BlockInventoryStocker.java
@@ -41,6 +41,70 @@ public class BlockInventoryStocker extends BlockContainer implements ITexturePro
 		}
 	}
 
+	public int getBlockTextureFromSideAndMetadata(int i, int m)
+	{
+		int dir = m & 7;
+
+		if (dir == i)
+		{
+			// front face
+			return 1;
+		}
+		if (dir <= 1) // Up or down
+		{
+			if (i >= 4)
+			{
+				return 18; // Side face
+			}
+			if (i >= 2)
+			{
+				return 16; // Top/bottom face
+			}
+			return 0; // Back face
+		}
+		if (i <= 1) 
+		{
+			return 16; // Horizontal, top or bottom face
+		}
+		if (dir == 2 || dir == 4)
+		{
+			if (i == 3 || i == 5)
+			{
+				return 18; // Side face
+			}
+		}
+		else
+		{
+			if (i == 2 || i == 4)
+			{
+				return 18; // Side face
+			}
+		}
+		return 0; // Back face
+	}
+
+    private int determineOrientation(World world, int x, int y, int z, EntityPlayer player)
+    {
+        if (player.rotationPitch > 45D)
+        {
+            return 1;
+        }
+
+        if (player.rotationPitch < -45D)
+        {
+            return 0;
+        }
+
+        int dir = MathHelper.floor_double((double)(player.rotationYaw * 4.0F / 360.0F) + 0.5D) & 3;
+        return dir == 0 ? 2 : (dir == 1 ? 5 : (dir == 2 ? 3 : (dir == 3 ? 4 : 0)));
+    }
+
+    public void onBlockPlacedBy(World world, int x, int y, int z, EntityLiving par5EntityLiving)
+    {
+        int dir = determineOrientation(world, x, y, z, (EntityPlayer)par5EntityLiving);
+        world.setBlockMetadataWithNotify(x, y, z, dir);
+    }
+
     @Override
     public boolean blockActivated(World world, int x, int y, int z, EntityPlayer entityplayer)
     {
@@ -59,7 +123,7 @@ public class BlockInventoryStocker extends BlockContainer implements ITexturePro
     
     public void onBlockPlaced(World world, int x, int y, int z, int facing)
     {
-    	TileEntity tile = world.getBlockTileEntity(x, y, z);
+    	// TileEntity tile = world.getBlockTileEntity(x, y, z);
     };
     
 	public void onBlockRemoval(World world, int x, int y, int z)
