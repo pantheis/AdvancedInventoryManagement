50d2a76f2427c3af3c88ab83b9eb79738f01f390
Yancarlo Ramsey <yancarlo@gmail.com>
2012-05-02 06:39:25 -0700

    Added textures.

--- src/minecraft/kaijin/InventoryStocker/BlockInventoryStocker.java
+++ src/minecraft/kaijin/InventoryStocker/BlockInventoryStocker.java
@@ -10,7 +10,7 @@ public class BlockInventoryStocker extends BlockContainer implements ITexturePro
 {
     public BlockInventoryStocker(int i, int j)
     {
-        super(i, j, Material.ground);
+        super(i, j, Material.iron);
     }
 
     public void addCreativeItems(ArrayList itemList)
@@ -20,13 +20,30 @@ public class BlockInventoryStocker extends BlockContainer implements ITexturePro
 
     public String getTextureFile()
     {
-        return "/Kaijin/StockerBlock/terrain.png";
+        return "/kaijin/InventoryStocker/terrain.png";
     }
+
+	public int getBlockTextureFromSide(int i)
+	{
+		// Needs to call tile entity to determine correct facing and animation state?
+		switch (i)
+		{
+		case 0: // Bottom
+			return 16;
+		case 1: // Top
+			return 16;
+		case 2: // North/South
+			return 1;
+		case 3: // North/South
+			return 0;
+		default: // 4-5 East/West
+			return 19;
+		}
+	}
 
     @Override
     public boolean blockActivated(World world, int x, int y, int z, EntityPlayer entityplayer)
     {
-//        	entityplayer.displayGUIChest((IInventory)tileentityinventorystocker);
         if (!Utils.isClient(world))
         {
             entityplayer.openGui(mod_InventoryStocker.instance, 1, world, x, y, z);
@@ -39,6 +56,11 @@ public class BlockInventoryStocker extends BlockContainer implements ITexturePro
     {
         return new TileEntityInventoryStocker();
     }
+    
+    public void onBlockPlaced(World world, int x, int y, int z, int facing)
+    {
+    	TileEntity tile = world.getBlockTileEntity(x, y, z);
+    };
     
 	public void onBlockRemoval(World world, int x, int y, int z)
 	{
