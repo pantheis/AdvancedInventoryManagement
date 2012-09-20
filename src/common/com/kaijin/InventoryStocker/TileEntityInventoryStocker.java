a1c5d2a0d5a0b3159fadba0ef23ebd0c213dcf15
Yancarlo Ramsey <yancarlo@gmail.com>
2012-09-19 19:09:15 -0700

    Revert "Fixed tube detection and light metainfo masking"
    This reverts commit aefe46e6d67efd9191103d154a4370d94c01139d.

--- src/common/com/kaijin/InventoryStocker/TileEntityInventoryStocker.java
+++ src/common/com/kaijin/InventoryStocker/TileEntityInventoryStocker.java
@@ -312,7 +312,7 @@ public class TileEntityInventoryStocker extends TileEntity implements IInventory
 			else if (type.endsWith("eloraam.base.BlockMicro"))
 			{
 				// RedPower Tube test
-				int m = worldObj.getBlockMetadata(x, y, z);
+				int m = this.Metainfo;
 
 				return (m >= 8) && (m <= 10);
 			}
@@ -1313,8 +1313,9 @@ public class TileEntityInventoryStocker extends TileEntity implements IInventory
 	private void lightsOff()
 	{
 		lightState = false;
-		int lights = this.Metainfo & 8; // Grab current state of lights
-		this.Metainfo ^= lights; // Toggles lights off if they're on (this two step method avoids worrying about retaining an unknown number of other bits)
+		int meta = this.Metainfo; // Grab current meta data
+		meta &= 7; // Clear bit 4
+		this.Metainfo = meta;
 		worldObj.markBlockNeedsUpdate(xCoord, yCoord, zCoord);
 //		sendExtraTEData(); // send packet to nearby clients informing them of the new light state
 	}
@@ -1323,7 +1324,9 @@ public class TileEntityInventoryStocker extends TileEntity implements IInventory
 	{
 		lightState = true;
 		// Turn on das blinkenlights!
-		this.Metainfo |= 8; // Turn lights on
+		int meta = this.Metainfo; // Grab current meta data
+		meta |= 8; // Set bit 4
+		this.Metainfo = meta;
 		worldObj.markBlockNeedsUpdate(xCoord, yCoord, zCoord);
 		if (Utils.isDebug())
 		{
